9f922fa163440c9c2e11515e371413feba916140
Franclin Lim <devfranclinjames@gmail.com>
2026-08-25 05:06:20 +0800

    fix css cold issue

--- src/custom.js
+++ src/custom.js
@@ -1,6 +1,17 @@
 // Find all items and add class to parent if text contains "cold"
-document.querySelectorAll('.item-name').forEach(item => {
-  if (item.textContent.toLowerCase().includes('cold')) {
-    item.closest('.item-btn')?.classList.add('skyblueme');
-  }
-});
+
+setInterval(function(){
+    var x = $('.classinit').attr('class');
+    if (x === undefined){
+    	//$( append script here).addClass('classinit');
+        document.querySelectorAll('.item-name').forEach(item => {
+        if (item.textContent.toLowerCase().includes('cold')) {
+            item.closest('.item-btn')?.classList.add('skyblueme classinit');
+        }
+        });
+    	console.log(x);
+    }
+    else {
+        return;
+    }
+},500);
