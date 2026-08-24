7334f52b33b22990ac0b9f0a1d5bab17cd175f6d
Franclin Lim <devfranclinjames@gmail.com>
2026-08-25 05:11:52 +0800

    fix css cold issue

--- src/custom.js
+++ src/custom.js
@@ -3,12 +3,14 @@
 setInterval(function(){
     var x = $('.classinit').attr('class');
     if (x === undefined){
-    	//$( append script here).addClass('classinit');
-        document.querySelectorAll('.item-name').forEach(item => {
-        if (item.textContent.toLowerCase().includes('cold')) {
-            item.closest('.item-btn')?.classList.add('skyblueme classinit');
-        }
-        });
+    	// Find all elements with class "item-name" that contain "cold" (case-insensitive)
+            $('.item-name').filter(function() {
+            return $(this).text().toLowerCase().includes('cold');
+            }).each(function() {
+            // Find the parent button with class "item-btn" and add the class
+            $(this).closest('.item-btn').addClass('skyblueme');
+            });
+        
     	console.log(x);
     }
     else {
