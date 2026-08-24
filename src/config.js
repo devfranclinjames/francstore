8e0dafd9bfd18777068de12fba1419dfc8c080c4
Franclin Lim <devfranclinjames@gmail.com>
2026-08-24 23:38:19 +0800

    fixupdate

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // (Deploy > New deployment > Web app > Execute as: Me > Who has access: Anyone)
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
 // ------------------------------------------------------------------
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbypofMo_zxJ75ERqYFluGBGss-ao3nJiWpMzrBAkVmfBpnMaVHz8LHfaCnR5c6wbkZN/exec'
+export const APPS_SCRIPT_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'
 
 export const STORE_NAME = 'Franc Store'
 export const GREETING = 'Daghang salamat sa pag palit, sunod napod!'
